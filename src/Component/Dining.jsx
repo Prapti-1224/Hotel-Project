6a255f0b9f7b3911788783e3f9f21c71ac0e7e16
prapti pokharel <praptipokhrel2@gmail.com>
2026-02-07 14:55:20 +0545

    Dining

--- src/Component/Dining.jsx
+++ src/Component/Dining.jsx
@@ -26,18 +26,8 @@ function RoomSuite() {
             className="w-130 h-70 object-cover rounded-xl shadow-md"
           />
           <h1 className="font-bold text-3xl text-[#5A4634] text-center">
-            Premium Room
+            Egg{" "}
           </h1>
-          <p className="text-xl text-center">
-            Step into a world of timeless elegance with our Heritage Deluxe
-            Rooms, where Nepal’s rich artistic traditions come to life.
-          </p>
-          <br />
-          <div className="text-center">
-            <button className="bg-[#463829] text-white px-5 py-2 rounded-full hover:bg-[#201b16] cursor-pointer duration-500 ease-in-out">
-              Book Now
-            </button>
-          </div>
         </div>
 
         <div className="flex flex-col items-center gap-4">
@@ -47,18 +37,8 @@ function RoomSuite() {
             className="w-120 h-70 object-cover rounded-xl shadow-md"
           />
           <h1 className="font-bold text-3xl text-[#5A4634] text-center">
-            Royal Suite
+            Thakali khana Set
           </h1>
-          <p className="text-xl text-center">
-            The Royal Suite at Dwarika’s Hotel is the pinnacle of heritage
-            luxury, offering an opulent retreat steeped in Nepal’s rich artistic
-            and architectural traditions.
-          </p>
-          <div className="text-center">
-            <button className="bg-[#463829] text-white px-5 py-2 rounded-full hover:bg-[#201b16] cursor-pointer duration-500 ease-in-out">
-              Book Now
-            </button>
-          </div>
         </div>
 
         <div className="flex flex-col items-center gap-4">
@@ -68,17 +48,8 @@ function RoomSuite() {
             className="w-120 h-70 object-cover rounded-xl shadow-md"
           />
           <h1 className="font-bold text-3xl text-[#5A4634] text-center">
-            Heritage Room
+            Momo
           </h1>
-          <p className="text-xl text-center">
-            It reflects traditional Newari-style architecture and preserves
-            Nepal’s cultural heritage.
-          </p>
-          <div className="text-center">
-            <button className="bg-[#463829] text-white px-5 py-2 rounded-full hover:bg-[#201b16] cursor-pointer duration-500 ease-in-out">
-              Book Now
-            </button>
-          </div>
         </div>
       </div>
 
@@ -90,18 +61,8 @@ function RoomSuite() {
             className="w-130 h-70 object-cover rounded-xl shadow-md"
           />
           <h1 className="font-bold text-3xl text-[#5A4634] text-center">
-            Executive Suite
+            Uramaki Sushi
           </h1>
-          <p className="text-xl text-center">
-            Immerse yourself in the grandeur of Nepal’s rich heritage with our
-            Executive Suite.
-          </p>
-          <br />
-          <div className="text-center">
-            <button className="bg-[#463829] text-white px-5 py-2 rounded-full hover:bg-[#201b16] cursor-pointer duration-500 ease-in-out">
-              Book Now
-            </button>
-          </div>
         </div>
 
         <div className="flex flex-col items-center gap-4">
@@ -111,18 +72,8 @@ function RoomSuite() {
             className="w-100 h-70 object-cover rounded-xl shadow-md"
           />
           <h1 className="font-bold text-3xl text-[#5A4634] text-center">
-            Deluxe Room
+            Rice Pudding
           </h1>
-          <p className="text-xl text-center">
-            Step into a world of timeless elegance with our Heritage Deluxe
-            Rooms, where Nepal’s rich artistic traditions come to life.
-          </p>
-
-          <div className="text-center">
-            <button className="bg-[#463829] text-white px-5 py-2 rounded-full hover:bg-[#201b16] cursor-pointer duration-500 ease-in-out">
-              Book Now
-            </button>
-          </div>
         </div>
 
         <div className="flex flex-col items-center gap-4">
@@ -132,18 +83,8 @@ function RoomSuite() {
             className="w-120 h-70 object-cover rounded-xl shadow-md"
           />
           <h1 className="font-bold text-3xl text-[#5A4634] text-center">
-            Junior Suite
+            Margarita
           </h1>
-          <p className="text-xl text-center">
-            Experience the grandeur of Nepal’s rich heritage in our Junior
-            Suites, where artistry and comfort come together in a timeless
-            embrace.
-          </p>
-          <div className="text-center">
-            <button className="bg-[#463829] text-white px-5 py-2 rounded-full hover:bg-[#201b16] cursor-pointer duration-500 ease-in-out">
-              Book Now
-            </button>
-          </div>
         </div>
       </div>
     </div>
